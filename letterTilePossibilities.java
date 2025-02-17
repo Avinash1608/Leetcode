8a14fe7ff4ffe0c45eace7c24d5687c822139ab5
Avinash1608 <aavinashracharla@gmail.com>
2025-02-17 22:00:37 +0530

    17/02/2025

---
+++ letterTilePossibilities.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    static int backtrack(int freq[]){
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            count++;
+            freq[i]--;
+            count+=backtrack(freq);
+            freq[i]++;
+        }
+
+        return count;
+    }
+    public int numTilePossibilities(String tiles) {
+
+        int len=tiles.length();
+        int freq[]=new int[26];
+        for(int i=0;i<len;i++){
+            freq[tiles.charAt(i)-'A']++;
+        }
+
+        int count=backtrack(freq);
+        return count;
+              
+    }
+}
