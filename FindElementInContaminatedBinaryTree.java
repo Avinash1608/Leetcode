a56523258274b0fcd2617347c6982b31ceb4f5ea
Avinash1608 <aavinashracharla@gmail.com>
2025-02-21 21:07:47 +0530

    21/02/2025

---
+++ FindElementInContaminatedBinaryTree.java
@@ -0,0 +1,19 @@
+class FindElements {
+
+    HashSet<Integer> set=new HashSet<>();
+    public FindElements(TreeNode root) {
+        dfs(root,0);
+    }
+    
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+    void dfs(TreeNode root,int val){
+        if(root==null) return;
+
+        root.val=val;
+        set.add(val);
+        dfs(root.left,2*val+1);
+        dfs(root.right,2*val+2);
+    }
+}
